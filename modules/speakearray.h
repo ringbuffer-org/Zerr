7d2fb275651bd2c088c06cd39a9fe2744ba2dea0
zeyuyang42 <213153676zsq@gmail.com>
2023-04-16 00:14:36 +0200

    speakerarray class

--- modules/speakearray.h
+++ modules/speakearray.h
@@ -6,24 +6,51 @@
 
 namespace zerr {
 /**
-* speaker array class with utilities
+* structure of speaker entity
 */
-class SpeakeArray {
+typedef struct Speaker {
+    int index;
+    float x;
+    float y;
+    float z;
+    float directness;
+    std::vector<int> contiguous;
+} Speaker;
+
+/**
+* speaker array class with utilities for manipulating speakers
+*/
+class SpeakerArray {
 public:
     // params
-    int n_speaker;
     std::vector<Speaker> speakers;
+    int n_speaker;
+    std::map<int, std::vector<int>> contiguous;
+
     // methods
-    Speaker get_speaker_by_index();
-    int
     // SpeakeArray();
+    void initialize(std::string config_path);
+    Speaker get_speaker_by_index(int speaker_index);
+    /**
+    * get one random speaker from the speaker array
+    */
+    int get_random_one_speaker();
+    /**
+    * get n random non-duplicated speakers from the speaker array
+    */
+    std::vector<int> get_random_speakers(int n);
+    int get_next_speaker(int speaker_index);
+    int get_
+    // 
     // ~SpeakeArray();
 
 private:
+    YAML::Node speaker_config;
     /**
-    * Load the 
+    * Load the YAML speaker config file
     */
-    void _read_config(std::string fileName);
+    void _read_config(std::string config_path);
+    void _init_contiguous_matrix();
 }; 
 
 } //namespace zerr
