bcc2cb5678f66cdc3b8d57f31688b10c3b101484
Zeyu Yang <213153676zsq@gmail.com>
2024-02-03 17:39:18 +0100

    init new file structure

--- core/include/utils/configs.h
+++ core/include/utils/configs.h
@@ -10,7 +10,8 @@
 #define TRIGGER_THRESHOLD 1e-4  /**< TODO  */
 
 #define PI 3.14159265           /**< TODO  */
-#define AUDIO_BUFFER_SIZE 1024  /**< TODO  */
+// #define AUDIO_BUFFER_SIZE 1024  /**< TODO  */
+#define AUDIO_BUFFER_SIZE 2048  /**< TODO  */
 #define VOLUME_THRESHOLD  1e-4  /**< TODO  */
 
 #define DISTANCE_SCALE    1e-1  /**< TODO  */
