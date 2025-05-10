605ea2d8a76add81802ce7f834c9c8b0b8e075e1
Zeyu Yang <213153676zsq@gmail.com>
2025-05-10 17:59:48 +0200

    add max/msp build automation

--- build.sh
+++ build.sh
@@ -60,6 +60,27 @@ build_puredata() {
     cd .. || exit 1
 }
 
+
+
+# -----------------------------------------------------------------------------
+build_maxmsp() {
+    echo "Building Zerr* for Max/MSP..."
+    cd maxmsp || { echo "Failed to enter 'puredata' directory"; exit 1; }
+
+    conan install . --output-folder=build --build=missing
+
+    cd build || { echo "Failed to enter 'maxmsp/build' directory"; exit 1; }
+
+    cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+    cmake --build .
+
+    echo "Installing zerr_core library to local lib folder..."
+    make install
+
+    cd ../.. || exit 1
+
+}
+
 # -----------------------------------------------------------------------------
 build_jack() {
     echo "Building Zerr* for JACK... (placeholder)"
@@ -85,6 +106,9 @@ for target in "$@"; do
     case $target in
         puredata)
             build_puredata
+            ;;
+        maxmsp)
+            build_maxmsp
             ;;
         jack)
             build_jack
